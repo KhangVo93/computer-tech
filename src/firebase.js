4eb05e6b78849185776dd7b372ed4c75e6d40e4f
vo khang <khangvlt@devcamp.edu.vn>
2022-03-24 21:31:51 +0700

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,20 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeDbs5ciNt5UmAZm5mnxNdzK4v1-6Sax4",
-  authDomain: "devcamp-r10-8f347.firebaseapp.com",
-  projectId: "devcamp-r10-8f347",
-  storageBucket: "devcamp-r10-8f347.appspot.com",
-  messagingSenderId: "239268509689",
-  appId: "1:239268509689:web:4b0ee86a4c8f0c89872f28"
+  // apiKey: "AIzaSyDeDbs5ciNt5UmAZm5mnxNdzK4v1-6Sax4",
+  // authDomain: "devcamp-r10-8f347.firebaseapp.com",
+  // projectId: "devcamp-r10-8f347",
+  // storageBucket: "devcamp-r10-8f347.appspot.com",
+  // messagingSenderId: "239268509689",
+  // appId: "1:239268509689:web:4b0ee86a4c8f0c89872f28"
+
+  apiKey: "AIzaSyCqYajgPDNbal69xX8gbnJOcN4M-QkGjts",
+  authDomain: "shop24-9c2a0.firebaseapp.com",
+  projectId: "shop24-9c2a0",
+  storageBucket: "shop24-9c2a0.appspot.com",
+  messagingSenderId: "222014938413",
+  appId: "1:222014938413:web:773e29315064872c1fc7a4",
+  measurementId: "G-GF7XKZ8C7Q"
 }
 
 firebase.initializeApp(firebaseConfig)
